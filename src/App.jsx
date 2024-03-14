b91f535cf303919b5eef50f4c1e14be7ade91659
19BCE0590-Niranjan <lakshminiranjanp@gmail.com>
2024-03-14 21:02:38 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Login_Signuppage from "./components/Login_Signuppage.jsx"
+
+
+function App() {
+
+
+  return (
+    <>
+    <Login_Signuppage/>
+    
+    </>
+
+  )
+}
+
+export default App
